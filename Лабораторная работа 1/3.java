0e2ff1cef288c250398d7bf04febde5980e55dc3
Maria Mikhailova <96312954+evoliatra@users.noreply.github.com>
2024-09-27 03:25:45 +0300

    Add files via upload

---
+++ Лабораторная работа 1/3.java
@@ -0,0 +1,16 @@
+public class third {
+    public static void main(String[] args) {
+int[] numbers = {1, 2, 3, 4, 5};
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Сумма элементов массива: " + sum);
+        System.out.println("Среднее арифметическое: " + average);
+    }
+}
+
